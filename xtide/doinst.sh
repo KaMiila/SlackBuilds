0e43a35d269dd3d987195611bbebcaa25427391b
KaMiila <lacielacie@rocketmail.com>
2020-02-05 08:22:24 -0800

    added post install library building command which fails during package compilation.

--- xtide/doinst.sh
+++ xtide/doinst.sh
@@ -7,3 +7,9 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+if [ -d /usr/lib64 ]; then
+    libtool --finish /usr/lib64
+else
+    libtool --finish /usr/lib
+fi
